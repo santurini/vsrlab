12de9489fe232b110b6d5bead2fda0c5ffa16fa1
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-01 14:15:22 +0200

    debug

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,17 +1,12 @@
 import logging
-import argparse
+
 import os
 from pathlib import Path
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--config_path', type=str, required=False, default='default', help='configuration path')
-args = parser.parse_args()
 
 logger = logging.getLogger(__name__)
 
 PROJECT_ROOT = Path.cwd().parents[0]
 LOGGING_DIR = Path(PROJECT_ROOT / 'storage')
-CONFIG_PATH = Path(args.config_path)
 
 logger.debug(f"Inferred project root: {PROJECT_ROOT}")
 os.environ["PROJECT_ROOT"] = str(PROJECT_ROOT)
