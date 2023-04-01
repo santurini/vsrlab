fe2fa866b2db6e06c214f5340fcb10f48c5ffa8f
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-01 14:15:51 +0200

    debug

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -11,5 +11,4 @@ LOGGING_DIR = Path(PROJECT_ROOT / 'storage')
 logger.debug(f"Inferred project root: {PROJECT_ROOT}")
 os.environ["PROJECT_ROOT"] = str(PROJECT_ROOT)
 os.environ["LOGGING_DIR"] = str(LOGGING_DIR)
-os.environ["CONFIG_PATH"] = str(CONFIG_PATH)
 os.environ["HYDRA_FULL_ERROR"] = "1"
