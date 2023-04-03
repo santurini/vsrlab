f531e3c9cf13575da897ea55ef7689557a4e5738
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-03 15:42:02 +0200

    debug

--- src/core/metrics.py
+++ src/core/metrics.py
@@ -1,13 +1,16 @@
 import torch.nn as nn
 
 class MetricCollection(nn.ModuleDict):
-    def __init__(self, metrics):
+    def __init__(self, metrics, prefix=None, postfix=None):
         super().__init__()
+        self.prefix = prefix
+        self.postfix = postfix
         self.add_metrics(metrics)
 
     def forward(self, *args):
         res = {k: m(*args).item() for k, m in self.items()}
-        return res
+        return {self._set_name(k): v for k, v in res.items()}
+
     def add_metrics(self, metrics: dict):
         for name in sorted(metrics.keys()):
             metric = metrics[name]
@@ -25,6 +28,16 @@ class MetricCollection(nn.ModuleDict):
                 for k, v in metric.items():
                     self[k] = v
 
-    def clone(self):
-        return deepcopy(self)
+    def clone(self, prefix: Optional[str] = None, postfix: Optional[str] = None):
+        mc = deepcopy(self)
+        if prefix:
+            mc.prefix = prefix
+        if postfix:
+            mc.postfix = postfix
+        return mc
+
+    def _set_name(self, base: str) -> str:
+        name = base if self.prefix is None else self.prefix + base
+        name = name if self.postfix is None else name + self.postfix
+        return name
 
