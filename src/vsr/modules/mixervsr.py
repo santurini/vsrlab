2f13a1719573756718a6b60619197977d21b9866
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-15 18:21:58 +0200

    testing baby_mixervsr

--- src/vsr/modules/mixervsr.py
+++ src/vsr/modules/mixervsr.py
@@ -16,7 +16,6 @@ class MixerVSR(nn.Module):
         channels_dim = 3 * patch_size ** 2
         patches_dim = (height // patch_size) * (width // patch_size)
         self.dcblock = DeformBlock(3, mid_ch, it_blocks)
-        #self.cleaner = IterativeRefinement(steps, 3, mid_ch, it_blocks)  # b t c h w
         self.encoder = EncoderDCT(patch_size)  # b t (h/p)*(w/p) (c*p*p)
         self.mixer = MlpMixer(patches_dim, channels_dim, time_dim, exp, mix_blocks)  # b t (h/p)*(w/p) (c*p*p)
         self.decoder = DecoderIDCT(mid_ch, patch_size, height, width)  # b t c h w
@@ -24,9 +23,8 @@ class MixerVSR(nn.Module):
 
     def forward(self, x):
         b, t, c, h, w = x.shape
-        #lq = self.cleaner(x)
-        #x_c = rearrange(lq, 'b t c h w -> (b t) c h w')
-        x_clean = x - self.dcblock(x.view(-1, c, h, w)).view(b, t, c, h, w)
+        x_noise = self.dcblock(x.view(-1, c, h, w))
+        x_clean = x - x_noise.view(b, t, c, h, w)
         x = self.encoder(x_clean)
         x = self.mixer(x)
         x = self.decoder(x)
@@ -34,22 +32,6 @@ class MixerVSR(nn.Module):
         up = F.interpolate(x_clean.view(-1, c, h, w), scale_factor=self.upscale, mode='bilinear')
         sr = rearrange(up, '(b t) c h w -> b t c h w', b=b, t=t) - x
         return sr, x_clean, None, None
-
-class IterativeRefinement(nn.Module):
-    def __init__(self, steps, *args, **kwargs):
-        super().__init__()
-        self.dcblock = DeformBlock(*args, **kwargs)
-        self.steps = steps
-
-    def forward(self, x):
-        b, t, c, h, w = x.shape
-        for _ in range(self.steps):
-            x = x.view(-1, c, h, w)
-            res = self.dcblock(x)
-            x = (x + res).view(b, t, c, h, w)
-            if torch.mean(torch.abs(res)) < 1e-3:
-                break
-        return x
 
 class SuperResolver(nn.Module):
     def __init__(self, in_ch, mid_ch, blocks, upscale):
