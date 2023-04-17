b147edf5142ae1514069ab0eb4e8e440ac8662b5
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-17 09:42:50 +0200

    testing baby_mixervsr_flow

--- src/vsr/pl_module/pl_module.py
+++ src/vsr/pl_module/pl_module.py
@@ -182,10 +182,10 @@ class LitVSR(pl.LightningModule):
 
     def log_images(self, out):
         b, t, c, h, w = out["sr"].shape
-        lr = resize(out["lr"][0][-1], (h, w)).cpu().detach()
-        lq = resize(out["lq"][0][-1], (h, w)).clamp(0, 1).cpu().detach()
-        hr = out["hr"][0][-1].cpu().detach()
-        sr = out["sr"][0][-1].clamp(0, 1).cpu().detach()
+        lr = resize(out["lr"][0][-1], (h, w)).detach()
+        lq = resize(out["lq"][0][-1], (h, w)).detach()
+        hr = out["hr"][0][-1].detach()
+        sr = out["sr"][0][-1].detach().clamp(0, 1)
 
         grid = make_grid([lr, hr, lq, sr], nrow=2, ncol=2)
         self.logger.log_image(key='Input Images', images=[grid], caption=[f'Model Output: step {self.global_step}'])
