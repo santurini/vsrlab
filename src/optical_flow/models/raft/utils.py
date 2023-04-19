5302ec0c39e18ebf845e8a97dfa0a613855956b3
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-19 14:53:46 +0200

    testing rr3db configuration

--- src/optical_flow/models/raft/utils.py
+++ src/optical_flow/models/raft/utils.py
@@ -8,7 +8,7 @@ def bilinear_sampler(img, coords, mask=False):
     xgrid = 2*xgrid/(W-1) - 1
     ygrid = 2*ygrid/(H-1) - 1
 
-    grid = torch.cat([xgrid, ygrid], dim=-1)
+    grid = torch.cat([xgrid, ygrid], dim=-1).type_as(img)
     img = F.grid_sample(img, grid, align_corners=True)
 
     if mask:
