7b9677a92ae9f58a59b73301d2a24ec2e8d6b4e7
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-19 15:16:02 +0200

    testing rr3db configuration

--- src/vsr/models/RR3DB/pl_module.py
+++ src/vsr/models/RR3DB/pl_module.py
@@ -182,9 +182,9 @@ class LitBase(pl.LightningModule):
 
     def log_images(self, out):
         b, t, c, h, w = out["sr"].shape
-        lr = resize(out["lr"][0][-1], (h, w)).detach()
-        hr = out["hr"][0][-1].detach()
-        sr = out["sr"][0][-1].detach().clamp(0, 1)
+        lr = resize(out["lr"][0, :, -1, :, :], (h, w)).detach()
+        hr = out["hr"][0, :, -1, :, :].detach()
+        sr = out["sr"][0, :, -1, :, :].detach().clamp(0, 1)
 
         grid = make_grid([lr, hr, sr], nrow=3, ncol=1)
         self.logger.log_image(key='Input Images', images=[grid], caption=[f'Model Output: step {self.global_step}'])
