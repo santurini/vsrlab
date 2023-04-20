0d81381b3e5a6768ae80ed0c02e8530d6aca814f
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-20 14:57:29 +0200

    testing rr3db

--- src/main.py
+++ src/main.py
@@ -39,6 +39,8 @@ def run(cfg: DictConfig) -> str:
     storage_dir: str = cfg.core.storage_dir
     logger: pl.loggers.Logger = hydra.utils.instantiate(cfg.train.logger)
 
+    logger.watch(model)
+
     strategy = hydra.utils.instantiate(cfg.train.strategy)
 
     pylogger.info("Instantiating the <Trainer>")
