af7b9aef7d4e7a8d4d5f4a46438e5882ce4b9875
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-21 10:00:54 +0200

    added video compression function

--- src/vsr/test.py
+++ src/vsr/test.py
@@ -31,9 +31,10 @@ def test(cfg: DictConfig) -> str:
 
     return
 
-@hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="default", version_base="1.3")
+@hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="test", version_base="1.3")
 def main(config: omegaconf.DictConfig):
-    run(config)
+    print(config)
+    #test(config)
 
 if __name__ == "__main__":
     main()
