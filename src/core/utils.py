738b1ebd6c80ae23b3bf23f99291abd4c0f63d5a
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-04-26 11:38:27 +0200

    testing

--- src/core/utils.py
+++ src/core/utils.py
@@ -84,7 +84,7 @@ def build_transform(cfg: ListConfig) -> List[Sequential]:
     augmentation = list()
     for aug in cfg:
         pylogger.info(f"Adding augmentation <{aug['_target_'].split('.')[-1]}>")
-        augmentation.append(hydra.utils.instantiate(aug, _recursive_=False, _convert_="all"))
+        augmentation.append(hydra.utils.instantiate(aug, _recursive_=False))
     return Sequential(*augmentation)
 
 def batched(iterable, n):
