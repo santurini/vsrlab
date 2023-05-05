cbd8af8cee86fbe6181936477faa8e680c8e37ef
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-05 15:15:16 +0200

    testing

--- src/train_torch.py
+++ src/train_torch.py
@@ -82,24 +82,25 @@ def run(cfg: DictConfig):
         for i, data in enumerate(train_dl):
             lr, hr = data[0].to(device), data[1].to(device)
 
-            with torch.cuda.amp.autocast():
-                sr, lq = model(lr)
-                loss_dict = compute_loss(loss_fn, loss_dict, sr, hr, lq)
-                loss = loss_dict["Loss"] / num_grad_acc
+            #with torch.cuda.amp.autocast():
+            sr, lq = model(lr)
+            loss_dict = compute_loss(loss_fn, loss_dict, sr, hr, lq)
+            loss = loss_dict["Loss"] / num_grad_acc
 
-                print("Loss:", loss_dict["Loss"].item())
-                metrics_dict = compute_metric(metric, metrics_dict, sr, hr)
+            print("Loss:", loss_dict["Loss"].item())
+            metrics_dict = compute_metric(metric, metrics_dict, sr, hr)
 
             print("Scaling Loss ...")
-            scaler.scale(loss).backward()
+            loss.backward()
+            #scaler.scale(loss).backward()
 
             if (i + 1) % num_grad_acc == 0:
                 print("Updating Parameters at Step {} ...".format(i))
-                scaler.step(optimizer)
-                scaler.update()
+                optimizer.step()
+                #scaler.step(optimizer)
+                #scaler.update()
                 #scheduler.step()
                 optimizer.zero_grad()
-
 
             #steps = update_weights(loss_dict["Loss"], scaler, scheduler, optimizer, num_grad_acc, steps, i, len(train_dl))
 
