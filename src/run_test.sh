4636ff7858f20d6090d9f3893e0724f56f1b368e
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-08 17:43:03 +0200

    testing

--- src/run_test.sh
+++ src/run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python test_torch.py +experiment=test
-python test_torch.py +experiment=test cfg_dir=/home/aghinassi/VSR/checkpoints/basic_cl_gan
-python test_torch.py +experiment=test cfg_dir=/home/aghinassi/VSR/checkpoints/basic_cl_gan2
+set -x
+
+python -u test_torch.py +experiment=test
+python -u test_torch.py +experiment=test cfg_dir=/home/aghinassi/VSR/checkpoints/basic_cl_gan
+python -u test_torch.py +experiment=test cfg_dir=/home/aghinassi/VSR/checkpoints/basic_cl_gan2
