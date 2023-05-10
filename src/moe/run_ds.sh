3dce08b08efca54667b72e91aca3a83bf5834a7c
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 19:03:24 +0200

    testing

---
+++ src/moe/run_ds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Number of nodes
+NUM_NODES=2
+# Number of GPUs per node
+NUM_GPUS=1
+
+deepspeed --num_nodes=${NUM_NODES} \
+          --hostfile /home/aghinassi/Desktop/nn-lab/src/moe/hostfile \
+          --master_addr 192.168.1.42 \
+          --master_port 1234 \
+          --num_gpus=${NUM_GPUS} \
+            cifar10_deepspeed.py \
+	        --log-interval 100 \
+	        --deepspeed \
+	        --deepspeed_config ./ds_config.json
