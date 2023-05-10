4025fd3490e97cdfd509c9324c54d9cf48ab0c76
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 20:00:45 +0200

    testing

--- src/moe/run_ds.sh
+++ src/moe/run_ds.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-deepspeed --master_addr localhost \
+deepspeed --master_addr 192.168.1.42 \
           --master_port 1234 \
           --no_ssh_check \
           --launcher OpenMPI \
+          --launcher_args '-np 2' \
             moe/cifar10_deepspeed.py \
 	        --log-interval 100 \
 	        --deepspeed \
