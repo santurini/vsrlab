6100fc93108a0697590d2f7de5475c39df796270
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 19:52:58 +0200

    testing

--- src/moe/run_ds.sh
+++ src/moe/run_ds.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-deepspeed --num_nodes 2 \
-          --num_gpus 1 \
-          --hostfile moe/hostfile \
-          --master_addr machine1 \
+deepspeed --hostfile moe/hostfile \
+          --master_addr 192.168.1.42 \
           --master_port 1234 \
           --no_ssh_check \
           --launcher OpenMPI \
