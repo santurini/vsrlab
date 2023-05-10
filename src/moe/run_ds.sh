eac7aa24eaa134690576c4f6abe40dd8893baf77
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 19:32:55 +0200

    testing

--- src/moe/run_ds.sh
+++ src/moe/run_ds.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Number of nodes
-NUM_NODES=2
-# Number of GPUs per node
-NUM_GPUS=1
-
 deepspeed --master_addr 192.168.1.42 \
           --master_port 1234 \
           --launcher OpenMPI \
