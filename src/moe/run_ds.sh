b3b9d8ab402206a8bd0c28cbe4407b68bb2b2499
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 20:20:41 +0200

    testing

--- src/moe/run_ds.sh
+++ src/moe/run_ds.sh
@@ -2,6 +2,7 @@
 
 deepspeed --hostfile moe/hostfile \
           --no_ssh_check \
+          --launcher OpenMPI \
             moe/cifar10_deepspeed.py \
 	        --deepspeed \
 	        --deepspeed_config moe/ds_config.json
