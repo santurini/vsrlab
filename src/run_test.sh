ba6782e5290865de9c8f7ce3a42646c8d785b911
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 22:44:10 +0200

    testing

--- src/run_test.sh
+++ src/run_test.sh
@@ -2,8 +2,8 @@
 
 set -x
 
+python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/basic_cl_gan window_size=128
+python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/basic_cl_gan2 window_size=128
+python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/basic_gan_lessadv window_size=128
 python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/vrt_gan
 python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/vrt_gan_of
-python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/basic_cl_gan
-python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/basic_cl_gan2
-python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/basic_gan_lessadv
