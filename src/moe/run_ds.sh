a8e3caef31302621db396ef9efada9076375f075
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 20:48:18 +0200

    testing

--- src/moe/run_ds.sh
+++ src/moe/run_ds.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-export PYTHONPATH=/home/aghinassi/miniconda3/envs/vsr/bin/python
-cd /home/aghinassi/Desktop/nn-lab/src
 
-deepspeed --hostfile moe/hostfile \
-          --master_addr rackete \
+deepspeed --num_nodes 2 \
+          --num_gpus 1 \
+          --hostfile moe/hostfile \
+          --master_addr 192.168.1.42 \
           --master_port 1234 \
-          --no_ssh_check \
             moe/cifar10_deepspeed.py \
 	        --deepspeed \
 	        --deepspeed_config moe/ds_config.json
