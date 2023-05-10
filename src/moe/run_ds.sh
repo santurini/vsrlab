4005ab1d97e97412bebfc0c54698975a19df0d24
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 19:45:53 +0200

    testing

--- src/moe/run_ds.sh
+++ src/moe/run_ds.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-deepspeed --num_nodes 2 \
-          --num_gpus 1 \
-          --hostfile moe/hostfile \
-          --master_addr machine1 \
+torchrun  --nnodes 2 \
+          --master_addr 192.168.1.42 \
           --master_port 1234 \
-            moe/cifar10_deepspeed.py \
+          python moe/cifar10_deepspeed.py \
 	        --log-interval 100 \
 	        --deepspeed \
 	        --deepspeed_config moe/ds_config.json
