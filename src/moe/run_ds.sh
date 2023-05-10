08099a5c5bf93d24d0f446245d7434c4103d0a68
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-10 19:19:48 +0200

    testing

--- src/moe/run_ds.sh
+++ src/moe/run_ds.sh
@@ -5,12 +5,10 @@ NUM_NODES=2
 # Number of GPUs per node
 NUM_GPUS=1
 
-deepspeed --num_nodes=${NUM_NODES} \
-          --hostfile /Users/arturo/Desktop/nn-lab/src/moe/hostfile \
+deepspeed --hostfile /Users/arturo/Desktop/nn-lab/src/moe/hostfile \
           --master_addr 192.168.1.42 \
           --master_port 1234 \
           --launcher OpenMPI \
-          --num_gpus=${NUM_GPUS} \
             cifar10_deepspeed.py \
 	        --log-interval 100 \
 	        --deepspeed \
