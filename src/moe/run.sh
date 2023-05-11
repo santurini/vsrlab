8fe2f286f90b0184689441af2831827d8c09fe2b
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-11 13:03:33 +0200

    testing

--- src/moe/run.sh
+++ src/moe/run.sh
@@ -4,5 +4,5 @@ deepspeed --hostfile=moe/hostfile \
           --master_addr=192.168.1.42 \
           --master_port=1234 \
           --launcher=OpenMPI \
-          --launcher_args='-bind-to none -map-by slot' \
+          --launcher_args='-bind-to none -map-by slot -x PATH=$PATH:/home/aghinassi/miniconda3/envs/vsr/bin' \
           moe/train.py --deepspeed --deepspeed_config "$(realpath moe/ds_config.json)"
