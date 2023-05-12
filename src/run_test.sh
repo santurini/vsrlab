1dcd8e7f014c26fcc56199b30ac84214c040688b
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-12 09:16:02 +0200

    testing

--- src/run_test.sh
+++ src/run_test.sh
@@ -2,5 +2,4 @@
 
 set -x
 
-python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/basic_cl_gan2 window_size=128 && killall python
 python -u test.py +experiment=test cfg_dir=/home/aghinassi/Desktop/checkpoints/basic_gan_lessadv window_size=128 && killall python
