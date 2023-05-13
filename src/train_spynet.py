c61fbf4cb8bc29cfe6429def00c43ffbecebb90b
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-13 17:14:25 +0200

    testing

--- src/train_spynet.py
+++ src/train_spynet.py
@@ -149,7 +149,7 @@ def train_one_level(cfg,
     train_dl, val_dl, _, _, epoch = build_loaders(cfg)
 
     current_level, trained_pyramid = build_spynets(cfg, k, previous, device)
-    optimizer, scheduler = build_optimizer(model, cfg.train.optimizer, cfg.train.scheduler)
+    optimizer, scheduler = build_optimizer(current_level, cfg.train.optimizer, cfg.train.scheduler)
     teacher = ptlflow.get_model(cfg.train.teacher.name, pretrained_ckpt=cfg.train.teacher.ckpt)
     cleaner = hydra.utils.instantiate(cfg.train.cleaner, _recursive_=False)
     cleaner.load_state_dict(
