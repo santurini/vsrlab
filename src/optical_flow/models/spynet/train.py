afdf5a0150ddf75a8ea0bff7825543a903abea10
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-13 15:10:24 +0200

    testing

--- src/optical_flow/models/spynet/train.py
+++ src/optical_flow/models/spynet/train.py
@@ -11,22 +11,71 @@ import optical_flow.models.spynet
 device = torch.device("cuda:{}".format(local_rank))
 denormalizer = Denormalize(mean=[.485, .406, .456],
                       std= [.229, .225, .224])
-def train_one_epoch(dl: DataLoader,
-                    optimizer: torch.optim.optimizer,
-                    scheduler: torch.optim.lr_scheduler,
-                    criterion_fn: torch.nn.Module,
-                    Gk: torch.nn.Module,
-                    teacher: torch.nn.Module,
-                    cleaner: torch.nn.Module,
-                    prev_pyramid: torch.nn.Module = None,
-                    epoch: int = 0,
-                    k: int = -1,
-                    size: tuple = None,
-                    logger: nn.Module = None
-                    ):
+
+@torch.no_grad()
+def evaluate(
+        cfg,
+        dl: DataLoader,
+        criterion_fn: torch.nn.Module,
+        Gk: torch.nn.Module,
+        teacher: torch.nn.Module,
+        cleaner: torch.nn.Module,
+        prev_pyramid: torch.nn.Module = None,
+        epoch: int = 0,
+        k: int = -1,
+        size: tuple = None,
+        logger: nn.Module = None
+):
+    model.eval()
+    val_loss = 0.0
+
+    if prev_pyramid is not None:
+        prev_pyramid.eval()
+
+    for i, data in enumerate(dl):
+        lr, hr = data[0].to(device), data[1].to(device)
+        x = get_frames(lr, cleaner, size)
+        y, hr = get_flow(hr, teacher, size)
+
+        if prev_pyramid is not None:
+            with torch.no_grad():
+                Vk_1 = prev_pyramid(x)
+                Vk_1 = F.interpolate(
+                    Vk_1, scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            Vk_1 = None
+
+        predictions = Gk(x, Vk_1, upsample_optical_flow=False)
+
+        if Vk_1 is not None:
+            y = y - Vk_1
+
+        loss = criterion_fn(y, predictions)
+
+        val_loss += loss.detach().item()
+
+    logger.log_dict({f"Loss {k}": val_loss / len(val_dl)}, epoch, "Val")
+    logger.log_flow(f"Val {k}", epoch, hr, denormalizer(x[0]), predictions, y)
+    save_k_checkpoint(cfg, k, current_level, logger, cfg.train.ddp)
+
+def train_one_epoch(
+        cfg,
+        dl: DataLoader,
+        optimizer: torch.optim.optimizer,
+        scheduler: torch.optim.lr_scheduler,
+        criterion_fn: torch.nn.Module,
+        Gk: torch.nn.Module,
+        teacher: torch.nn.Module,
+        cleaner: torch.nn.Module,
+        prev_pyramid: torch.nn.Module = None,
+        epoch: int = 0,
+        k: int = -1,
+        size: tuple = None,
+        logger: nn.Module = None
+):
     Gk.train()
     dt = time.time()
-    running_loss = 0.
+    train_loss = 0.
 
     if prev_pyramid is not None:
         prev_pyramid.eval()
@@ -52,10 +101,15 @@ def train_one_epoch(dl: DataLoader,
         loss = criterion_fn(y, predictions)
         update_weights(loss, scheduler, optimizer)
 
-        running_loss += loss.detach().item()
+        train_loss += loss.detach().item()
 
     logger.log_dict({f"Loss {k}": train_loss / len(train_dl)}, epoch, f"Train")
     logger.log_flow(f"Train {k}", epoch, hr, denormalizer(x[0]), predictions, y)
+
+    evaluate(
+        cfg, train_dl, loss_fn, current_level, teacher,
+        cleaner, trained_pyramid, epoch, k, size, logger
+    )
 
     dt = time.time() - dt
     print(f"Epoch {epoch} Level {k} - Elapsed time --> {dt:2f}")
@@ -78,21 +132,21 @@ def train_one_level(cfg,
     size = spynet.config.GConf(k).image_size
 
     for epoch in range(cfg.train.max_epochs[k]):
-        train_one_epoch(train_dl,
-                        optimizer,
-                        scheduler,
-                        loss_fn,
-                        current_level,
-                        teacher,
-                        cleaner,
-                        trained_pyramid,
-                        epoch,
-                        k,
-                        size,
-                        logger
-                        )
-
-    save_k_checkpoint(cfg, k, current_level, logger, cfg.train.ddp)
+        train_one_epoch(
+            cfg,
+            train_dl,
+            optimizer,
+            scheduler,
+            loss_fn,
+            current_level,
+            teacher,
+            cleaner,
+            trained_pyramid,
+            epoch,
+            k,
+            size,
+            logger
+            )
     
     return current_level
 
