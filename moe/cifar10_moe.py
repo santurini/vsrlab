19a39ea7d0f29ae5c40f2ee616b27c60b00f8b61
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-13 20:18:37 +0200

    testing

--- moe/cifar10_moe.py
+++ moe/cifar10_moe.py
@@ -127,20 +127,22 @@ args = add_argument()
 class Mlp_GEGLU(nn.Module):
     """ Multilayer perceptron with gated linear unit (GEGLU)"""
 
-    def __init__(self, in_features, out_features=None, act_layer=nn.GELU, drop=0.):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
-        out_features = out_features
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
 
-        self.fc11 = nn.Linear(in_features, in_features)
-        self.fc12 = nn.Linear(in_features, in_features)
+        self.fc11 = nn.Linear(in_features, hidden_features)
+        self.fc12 = nn.Linear(in_features, hidden_features)
         self.act = act_layer()
-        self.fc2 = nn.Linear(in_features, out_features)
+        self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
 
     def forward(self, x):
         x = self.act(self.fc11(x)) * self.fc12(x)
         x = self.drop(x)
         x = self.fc2(x)
+
         return x
 
 class Net(nn.Module):
@@ -152,7 +154,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         if args.moe:  # changed here
-            fc3 = Mlp_GEGLU(84, 84)
+            fc3 = Mlp_GEGLU(84, 128, 84)
             self.experts = deepspeed.moe.layer.MoE(hidden_size=84, expert=fc3, num_experts=args.num_experts_per_layer,
                                                    ep_size=args.ep_world_size, use_residual=args.mlp_type == 'residual',
                                                    k=args.top_k, min_capacity=args.min_capacity,
