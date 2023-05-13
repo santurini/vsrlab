e4856b09cd892b851c818386f050026b1caf00db
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-13 17:01:42 +0200

    testing

--- src/train_spynet.py
+++ src/train_spynet.py
@@ -23,7 +23,7 @@ from optical_flow.models.spynet.utils import (
 from core import PROJECT_ROOT
 from core.utils import build_optimizer, save_checkpoint, cleanup
 
-device = torch.device("cuda:{}".format(os.environ["LOCAL_RANK"]))
+device = torch.device("cuda:{}".format(0))
 denormalizer = Denormalize(mean=[.485, .406, .456],
                       std= [.229, .225, .224])
 
