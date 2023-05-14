6f3298fb879c322b119e62b40c8344116bc3955a
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-14 13:10:49 +0200

    testing

--- src/run_moe.sh
+++ src/run_moe.sh
@@ -6,4 +6,4 @@ deepspeed --hostfile="$PROJECT_ROOT/vsr/MoEVRT/hostfile" \
           --master_port=1234 \
           --launcher=openmpi \
           --launcher_args="-bind-to none -map-by slot -x PATH=$PATH:/home/aghinassi/miniconda3/envs/vsr/bin" \
-            train_moe.py --deepspeed --deepspeed_config "$PROJECT_ROOT/vsr/MoEVRT/ds_config.json"
+            train_moe.py +experiment=vrt_moe --deepspeed --deepspeed_config "$PROJECT_ROOT/vsr/MoEVRT/ds_config.json"
