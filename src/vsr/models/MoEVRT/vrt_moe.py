204751b955f09be713b568368c307d60cd4adfca
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-14 14:00:12 +0200

    testing

--- src/vsr/models/MoEVRT/vrt_moe.py
+++ src/vsr/models/MoEVRT/vrt_moe.py
@@ -97,8 +97,8 @@ class TinyVRT(nn.Module):
 
         self.iterative_refinement = IterativeRefinement(
             refine_ch,
-            refine_steps,
             refine_blocks,
+            refine_steps
         )
 
         # conv_first
