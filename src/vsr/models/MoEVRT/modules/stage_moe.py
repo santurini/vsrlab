02052f1743d50b26d805e94c715ae8926d1a7c86
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-14 16:36:08 +0200

    testing

--- src/vsr/models/MoEVRT/modules/stage_moe.py
+++ src/vsr/models/MoEVRT/modules/stage_moe.py
@@ -117,8 +117,8 @@ class Stage(nn.Module):
 
         x = x.transpose(1, 2)
         x_backward, x_forward = self.get_aligned_features(x, flows_backward, flows_forward)
-        x = self.pa_fuse(torch.cat([x, x_backward, x_forward], 2).permute(0, 1, 3, 4, 2)) \
-                print(x.shape)
+        x, _, _ = self.pa_fuse(torch.cat([x, x_backward, x_forward], 2).permute(0, 1, 3, 4, 2))
+        print(x.shape)
         print(x.permute(0, 4, 1, 2, 3).shape)
         return self.linear3(x)
 
