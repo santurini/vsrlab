379e32b79e6f085acf5a87f47257818ac72a6d5f
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-15 11:55:42 +0200

    testing

--- src/moe/MoEVRT/deepspeed/vrt_moe.py
+++ src/moe/MoEVRT/deepspeed/vrt_moe.py
@@ -24,7 +24,8 @@ class Debugger(nn.Module):
         self.rank = rank
 
     def forward(self, x):
-        print("IM HERE:", x.shape)
+        if self.rank == 0:
+            print("IM HERE:", x.shape)
         return x
 
 class Upsample(nn.Sequential):
