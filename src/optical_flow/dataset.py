930ef10eb927ff8d91ff37af3b1f20571ca42b61
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-16 11:15:26 +0200

    testing

--- src/optical_flow/dataset.py
+++ src/optical_flow/dataset.py
@@ -3,7 +3,6 @@ from typing import Union
 
 import torch
 from PIL import Image
-from einops import rearrange
 from torchvision.transforms.functional import to_tensor
 
 class Dataset(torch.utils.data.Dataset):
@@ -33,11 +32,11 @@ class Dataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx: int):
         of_path = self.path[idx]
-        supp, ref, optical_flow = self.get_path(of_path)
-        supp = to_tensor(Image.open(supp))
-        ref = to_tensor(Image.open(ref))
+        frame1, frame2, optical_flow = self.get_path(of_path)
+        frame1 = to_tensor(Image.open(frame1))
+        frame2 = to_tensor(Image.open(frame2))
 
-        sequence = torch.stack([ref, supp])
+        sequence = torch.stack([frame1, frame2])
 
         sequence, optical_flow = self.augmentation(sequence, optical_flow)
         sequence, optical_flow = self.compression(sequence, optical_flow)
@@ -45,13 +44,10 @@ class Dataset(torch.utils.data.Dataset):
         return sequence[0], sequence[1], optical_flow
 
     def get_path(self, path):
-        optical_flow = rearrange(
-            torch.load(path, map_location="cpu"),
-            '1 1 1 1 c h w -> c h w'
-        )
+        optical_flow = torch.load(path, map_location="cpu")
         path = str(path.stem).split('_')
         video_name = '_'.join(path[:2])
-        supp = list((self.root / video_name).glob(f"{path[-2]}.*"))[0]
-        ref = list((self.root / video_name).glob(f"{Path(path[-1]).stem}.*"))[0]
+        frame1 = list((self.root / video_name).glob(f"{path[-2]}.*"))[0]
+        frame2 = list((self.root / video_name).glob(f"{Path(path[-1]).stem}.*"))[0]
 
-        return supp, ref, optical_flow
+        return frame1, frame2, optical_flow
