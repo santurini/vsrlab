69f58a4dec38d83ac8591c8cd373bf550cde2194
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-16 16:58:21 +0200

    testing

--- src/moe/MoEVRT/deepspeed/vrt_moe.py
+++ src/moe/MoEVRT/deepspeed/vrt_moe.py
@@ -146,7 +146,7 @@ class TinyVRT(nn.Module):
 
         # last stage
         self.stage6 = deepspeed.moe.layer.MoE(
-            hidden_size=embed_dims[len(scales) - 1],
+            hidden_size=image_size[1],
             expert=nn.Sequential(*
                                  [
                                      Debug(),
