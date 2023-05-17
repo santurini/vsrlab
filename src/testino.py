e9864f8acc3a54be288dd58aa1ce8442c45b8a27
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-17 12:02:21 +0200

    testing

--- src/testino.py
+++ src/testino.py
@@ -39,7 +39,7 @@ MoE = deepspeed.moe.layer.MoE(
     expert=nn.Sequential(*
                          [
                              Debug("MoE Input"),
-                             Rearrange('n 1 (h w e) (c d)-> n d h w (c e)', d=window_size[0], h=img_size[1],
+                             Rearrange('n 1 (h w e) (c d)-> n d h w (c e)', d=window_size[0], h=img_size[1], e=top_k,
                                        c=embed_dims[len(scales) - 1]),
                              Debug("After Rearrange"),
                              nn.LayerNorm(embed_dims[len(scales) - 1]),
