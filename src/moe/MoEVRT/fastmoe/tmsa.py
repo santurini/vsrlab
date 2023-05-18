b1e664fc9f4b2aef0ca40a34c64a19ce2baf2144
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-18 14:14:18 +0200

    testing

---
+++ src/moe/MoEVRT/fastmoe/tmsa.py
@@ -0,0 +1,280 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+from fmoe.gates.swipe_gate import SwipeGate
+from fmoe.transformer import FMoETransformerMLP
+from vsr.models.VRT.modules.stochastic_depth import DropPath
+from vsr.models.VRT.modules.window_attention import *
+
+class TMSA(nn.Module):
+    """ Temporal Mutual Self Attention (TMSA).
+
+    Args:
+        dim (int): Number of input channels.
+        input_resolution (tuple[int]): Input resolution.
+        num_heads (int): Number of attention heads.
+        window_size (tuple[int]): Window size.
+        shift_size (tuple[int]): Shift size for mutual and self attention.
+        mut_attn (bool): If True, use mutual and self attention. Default: True.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True.
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop_path (float, optional): Stochastic depth rate. Default: 0.0.
+        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU.
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm.
+        use_checkpoint_attn (bool): If True, use torch.checkpoint for attention modules. Default: False.
+        use_checkpoint_ffn (bool): If True, use torch.checkpoint for feed-forward modules. Default: False.
+    """
+
+    def __init__(self,
+                 dim,
+                 input_resolution,
+                 num_heads,
+                 num_experts,
+                 num_gpus,
+                 top_k,
+                 gate=SwipeGate,
+                 window_size=(6, 8, 8),
+                 shift_size=(0, 0, 0),
+                 mut_attn=True,
+                 mlp_ratio=2.,
+                 qkv_bias=True,
+                 qk_scale=None,
+                 drop_path=0.,
+                 act_layer=nn.GELU,
+                 norm_layer=nn.LayerNorm
+                 ):
+        super().__init__()
+        self.dim = dim
+        self.input_resolution = input_resolution
+        self.num_heads = num_heads
+        self.window_size = window_size
+        self.shift_size = shift_size
+
+        assert 0 <= self.shift_size[0] < self.window_size[0], "shift_size must be in range [0, window_size]"
+        assert 0 <= self.shift_size[1] < self.window_size[1], "shift_size must be in range [0, window_size]"
+        assert 0 <= self.shift_size[2] < self.window_size[2], "shift_size must be in range [0, window_size]"
+
+        self.norm1 = norm_layer(dim)
+        self.attn = WindowAttention(dim, window_size=self.window_size, num_heads=num_heads,
+                                    qkv_bias=qkv_bias, qk_scale=qk_scale, mut_attn=mut_attn)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        self.mlp = FMoETransformerMLP(
+            num_expert=num_experts,
+            d_model=dim,
+            d_hidden=int(dim * mlp_ratio),
+            activation=act_layer,
+            expert_rank=os.environ.get("RANK", 0),
+            world_size=num_gpus,
+            top_k=top_k,
+            gate=gate
+        )
+
+    def forward_part1(self, x, mask_matrix):
+        B, D, H, W, C = x.shape
+        window_size, shift_size = get_window_size((D, H, W), self.window_size, self.shift_size)
+
+        x = self.norm1(x)
+
+        # pad feature maps to multiples of window size
+        pad_l = pad_t = pad_d0 = 0
+        pad_d1 = (window_size[0] - D % window_size[0]) % window_size[0]
+        pad_b = (window_size[1] - H % window_size[1]) % window_size[1]
+        pad_r = (window_size[2] - W % window_size[2]) % window_size[2]
+        x = F.pad(x, (0, 0, pad_l, pad_r, pad_t, pad_b, pad_d0, pad_d1), mode='constant')
+
+        _, Dp, Hp, Wp, _ = x.shape
+
+        # cyclic shift
+        if any(i > 0 for i in shift_size):
+            shifted_x = torch.roll(x, shifts=(-shift_size[0], -shift_size[1], -shift_size[2]), dims=(1, 2, 3))
+            attn_mask = mask_matrix
+        else:
+            shifted_x = x
+            attn_mask = None
+
+        # partition windows
+        x_windows = window_partition(shifted_x, window_size)  # B*nW, Wd*Wh*Ww, C
+
+        # attention / shifted attention
+        attn_windows = self.attn(x_windows, mask=attn_mask)  # B*nW, Wd*Wh*Ww, C
+
+        # merge windows
+        attn_windows = attn_windows.view(-1, *(window_size + (C,)))
+        shifted_x = window_reverse(attn_windows, window_size, B, Dp, Hp, Wp)  # B D' H' W' C
+
+        # reverse cyclic shift
+        if any(i > 0 for i in shift_size):
+            x = torch.roll(shifted_x, shifts=(shift_size[0], shift_size[1], shift_size[2]), dims=(1, 2, 3))
+        else:
+            x = shifted_x
+
+        if pad_d1 > 0 or pad_r > 0 or pad_b > 0:
+            x = x[:, :D, :H, :W, :]
+
+        x = self.drop_path(x)
+
+        return x
+
+    def forward_part2(self, x):
+        return self.drop_path(self.mlp(self.norm2(x)))
+
+    def forward(self, x, mask_matrix):
+        """ Forward function.
+
+        Args:
+            x: Input feature, tensor size (B, D, H, W, C).
+            mask_matrix: Attention mask for cyclic shift.
+        """
+
+        # attention
+        x = x + self.forward_part1(x, mask_matrix)
+        x = x + self.forward_part2(x)
+
+        return x
+
+class TMSAG(nn.Module):
+    """ Temporal Mutual Self Attention Group (TMSAG).
+
+    Args:
+        dim (int): Number of feature channels
+        input_resolution (tuple[int]): Input resolution.
+        depth (int): Depths of this stage.
+        num_heads (int): Number of attention head.
+        window_size (tuple[int]): Local window size. Default: (6,8,8).
+        shift_size (tuple[int]): Shift size for mutual and self attention. Default: None.
+        mut_attn (bool): If True, use mutual and self attention. Default: True.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim. Default: 2.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop_path (float | tuple[float], optional): Stochastic depth rate. Default: 0.0
+        norm_layer (nn.Module, optional): Normalization layer. Default: nn.LayerNorm
+        use_checkpoint_attn (bool): If True, use torch.checkpoint for attention modules. Default: False.
+        use_checkpoint_ffn (bool): If True, use torch.checkpoint for feed-forward modules. Default: False.
+    """
+
+    def __init__(self,
+                 dim,
+                 input_resolution,
+                 depth,
+                 num_heads,
+                 num_experts,
+                 num_gpus,
+                 top_k,
+                 gate=SwipeGate,
+                 window_size=[6, 8, 8],
+                 shift_size=None,
+                 mut_attn=True,
+                 mlp_ratio=2.,
+                 qkv_bias=False,
+                 qk_scale=None,
+                 drop_path=0.,
+                 norm_layer=nn.LayerNorm,
+                 ):
+        super().__init__()
+        self.input_resolution = input_resolution
+        self.window_size = window_size
+        self.shift_size = list(i // 2 for i in window_size) if shift_size is None else shift_size
+
+        # build blocks
+        self.blocks = nn.ModuleList([
+            TMSA(
+                dim=dim,
+                input_resolution=input_resolution,
+                num_heads=num_heads,
+                num_experts=num_experts,
+                num_gpus=num_gpus,
+                top_k=top_k,
+                gate=gate,
+                window_size=window_size,
+                shift_size=[0, 0, 0] if i % 2 == 0 else self.shift_size,
+                mut_attn=mut_attn,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                qk_scale=qk_scale,
+                drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path,
+                norm_layer=norm_layer
+            )
+            for i in range(depth)])
+
+    def forward(self, x):
+        """ Forward function.
+
+        Args:
+            x: Input feature, tensor size (B, C, D, H, W).
+        """
+        # calculate attention mask for attention
+        B, C, D, H, W = x.shape
+        window_size, shift_size = get_window_size((D, H, W), self.window_size, self.shift_size)
+        x = rearrange(x, 'b c d h w -> b d h w c')
+        Dp = int(np.ceil(D / window_size[0])) * window_size[0]
+        Hp = int(np.ceil(H / window_size[1])) * window_size[1]
+        Wp = int(np.ceil(W / window_size[2])) * window_size[2]
+        attn_mask = compute_mask(Dp, Hp, Wp, window_size, shift_size, x.device).type_as(x)
+
+        for blk in self.blocks:
+            x = blk(x, attn_mask)
+        x = x.view(B, D, H, W, -1)
+        x = rearrange(x, 'b d h w c -> b c d h w')
+
+        return x
+
+class RTMSA(nn.Module):
+    """ Residual Temporal Mutual Self Attention (RTMSA). Only used in stage 8.
+
+    Args:
+        dim (int): Number of input channels.
+        input_resolution (tuple[int]): Input resolution.
+        depth (int): Number of blocks.
+        num_heads (int): Number of attention heads.
+        window_size (int): Local window size.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True.
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop_path (float | tuple[float], optional): Stochastic depth rate. Default: 0.0.
+        norm_layer (nn.Module, optional): Normalization layer. Default: nn.LayerNorm.
+    """
+
+    def __init__(self,
+                 dim,
+                 input_resolution,
+                 depth,
+                 num_heads,
+                 num_experts,
+                 num_gpus,
+                 top_k,
+                 window_size,
+                 mlp_ratio=2.,
+                 qkv_bias=True,
+                 qk_scale=None,
+                 drop_path=0.,
+                 norm_layer=nn.LayerNorm,
+                 gate=SwipeGate,
+                 ):
+        super().__init__()
+        self.dim = dim
+        self.input_resolution = input_resolution
+
+        self.residual_group = TMSAG(dim=dim,
+                                    input_resolution=input_resolution,
+                                    depth=depth,
+                                    num_heads=num_heads,
+                                    window_size=window_size,
+                                    num_experts=num_experts,
+                                    num_gpus=num_gpus,
+                                    top_k=top_k,
+                                    gate=gate,
+                                    mut_attn=False,
+                                    mlp_ratio=mlp_ratio,
+                                    qkv_bias=qkv_bias, qk_scale=qk_scale,
+                                    drop_path=drop_path,
+                                    norm_layer=norm_layer,
+                                    )
+
+        self.linear = nn.Linear(dim, dim)
+
+    def forward(self, x):
+        return x + self.linear(self.residual_group(x).transpose(1, 4)).transpose(1, 4)
