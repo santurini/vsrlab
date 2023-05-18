0e646ff33060b4f05a66543884e2c6eaff8aad23
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-18 18:48:59 +0200

    testing

--- src/train.py
+++ src/train.py
@@ -81,8 +81,11 @@ def run(cfg: DictConfig):
             sr, lq = model(lr)
             loss = compute_loss(loss_fn, sr, hr, lq)
 
-            update_weights(model, loss, scaler, scheduler,
-                           optimizer, num_grad_acc, gradient_clip_val, i)
+            # update_weights(model, loss, scaler, scheduler,
+            #               optimizer, num_grad_acc, gradient_clip_val, i)
+
+            update_weights_basic(model, loss, scheduler,
+                                 optimizer, num_grad_acc, gradient_clip_val, i)
 
             train_loss += loss.detach().item()
             train_metrics = running_metrics(train_metrics, metric, sr, hr)
