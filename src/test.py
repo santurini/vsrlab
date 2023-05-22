5c9bbeec18c817d86f7c7b3567af84828e4b0a71
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-22 12:03:29 +0200

    testing

--- src/test.py
+++ src/test.py
@@ -1,20 +1,25 @@
+import os
 import time
 import warnings
 from concurrent.futures import ThreadPoolExecutor
 
+import hydra
 import omegaconf
 import pandas as pd
 from torchvision.utils import save_image
 
 from core import PROJECT_ROOT
-from core.utils import *
+from core.utils import (
+    build_model,
+    build_metric,
+    get_video,
+    running_metrics
+)
 
 warnings.filterwarnings('ignore')
 pylogger = logging.getLogger(__name__)
 
-BPP = {
-
-}
+C, H, W = 3, 480, 640
 
 @torch.no_grad()
 def run(config):
@@ -41,7 +46,7 @@ def run(config):
             video_folder = os.path.join(config.lr_dir, f"fps={fps}_crf={crf}", "frames")
             output_folder = os.path.join(config.out_dir, os.path.basename(config.cfg_dir))
             video_paths = list(Path(video_folder).glob('*'))
-            video_metrics, bpp = {k: 0 for k in config.metric.metrics}, 0
+            video_metrics, bpp, cf = {k: 0 for k in config.metric.metrics}, 0, 0
 
             for video_lr_path in video_paths:
                 model.eval()
@@ -55,9 +60,13 @@ def run(config):
                 video_hr, video_lr = get_video(video_hr_path, pool).to(device), \
                     get_video(video_lr_path, pool).to(device)
 
-                _, n_frames, c, h, w = video_hr.shape
-                size_bits = (Path(config.hr_dir) / f"fps={fps}_crf=5" / "video" / video_name).stat().st_size * 8
-                bpp += size_bits / (c * h * w * n_frames)
+                F = video_hr.size(1)
+                size_bits_orig = (Path(config.hr_dir) / f"fps={fps}_crf=5" / "video" / video_name).stat().st_size * 8
+                size_bits_comp = (Path(
+                    config.lr_dir) / f"fps={fps}_crf={crf}" / "video" / video_name).stat().st_size * 8
+
+                cf += size_bits_comp / size_bits_orig
+                bpp += size_bits_comp / (C * H * W * F)
 
                 outputs = []
                 for i in range(0, video_lr.size(1), config.window_size):
@@ -80,8 +89,9 @@ def run(config):
                 print(f"Inference Time --> {dt:2f}")
 
             video_pd.append(
-                {"bpp": bpp / len(video_paths), "fps": fps, "crf": crf} | {k: v / len(video_paths) for k, v in
-                                                                           video_metrics.items()})
+                {"cf": cf / len(video_paths), "bpp": bpp / len(video_paths), "fps": fps, "crf": crf} | {
+                    k: v / len(video_paths) for k, v in
+                    video_metrics.items()})
 
     pd.DataFrame(video_pd).to_csv(os.path.join(output_folder, f'{os.path.basename(config.cfg_dir)}.csv'))
 
