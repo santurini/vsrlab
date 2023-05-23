439e74cf9e45b2adda0f5e01b60034681cf74692
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-05-23 20:17:40 +0200

    testing

--- src/train_moe.py
+++ src/train_moe.py
@@ -90,6 +90,7 @@ def run(cfg: omegaconf.DictConfig):
         train_loss, train_metrics = 0.0, {k: 0 for k in cfg.train.metric.metrics}
 
         for i, data in enumerate(train_dl):
+            print("batch {}/{}".format(i, len(train_dl)))
             lr, hr = data[0].to(device), data[1].to(device)
 
             with torch.cuda.amp.autocast():
