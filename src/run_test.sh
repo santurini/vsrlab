fb7e157049e9169bcbb6c075778f6b3a9809d84e
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-06-02 16:46:51 +0200

    testing

--- src/run_test.sh
+++ src/run_test.sh
@@ -6,8 +6,8 @@ set -e -x
 # python test.py +experiment=test cfg_dir=../../checkpoints/basic_cl_gan
 # python test.py +experiment=test cfg_dir=../../checkpoints/basic_cl_gan2
 # python test.py +experiment=test cfg_dir=../../checkpoints/basic_spynet
-python test.py +experiment=test cfg_dir=../../checkpoints/vrt
-# python test.py +experiment=test cfg_dir=../../checkpoints/vrt_cl
+# python test.py +experiment=test cfg_dir=../../checkpoints/vrt
+python test.py +experiment=test cfg_dir=../../checkpoints/vrt_cl
 # python test.py +experiment=test cfg_dir=../../checkpoints/vrt_spynet
 # python test.py +experiment=test cfg_dir=../../checkpoints/moe
 # python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl
