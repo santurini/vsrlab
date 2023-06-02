c0cc15517fb4583b0c505b9bc4bb1e93d68ff886
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-06-02 15:49:07 +0200

    testing

--- src/run_test.sh
+++ src/run_test.sh
@@ -7,12 +7,12 @@ set -e -x
 # python test.py +experiment=test cfg_dir=../../checkpoints/basic_cl_gan2
 # python test.py +experiment=test cfg_dir=../../checkpoints/basic_spynet
 python test.py +experiment=test cfg_dir=../../checkpoints/vrt
-python test.py +experiment=test cfg_dir=../../checkpoints/vrt_cl
-python test.py +experiment=test cfg_dir=../../checkpoints/vrt_spynet
-python test.py +experiment=test cfg_dir=../../checkpoints/moe
-python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl
-python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl2
-python test.py +experiment=test cfg_dir=../../checkpoints/moe_spynet
-python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e
-python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e_cl
-python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e_cl2
+# python test.py +experiment=test cfg_dir=../../checkpoints/vrt_cl
+# python test.py +experiment=test cfg_dir=../../checkpoints/vrt_spynet
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl2
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_spynet
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e_cl
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e_cl2
