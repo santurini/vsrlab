ae39d6b41797c14cdd5d31ca2f5acd8ae7ba9338
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-06-02 21:11:03 +0200

    testing

--- src/run_test.sh
+++ src/run_test.sh
@@ -10,9 +10,9 @@ set -e -x
 # python test.py +experiment=test cfg_dir=../../checkpoints/vrt_cl
 # python test.py +experiment=test cfg_dir=../../checkpoints/vrt_spynet
 # python test.py +experiment=test cfg_dir=../../checkpoints/moe
-python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl
-python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl2
-# python test.py +experiment=test cfg_dir=../../checkpoints/moe_spynet
-# python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e
-# python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e_cl
-# python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e_cl2
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_cl2
+# python test.py +experiment=test cfg_dir=../../checkpoints/moe_spynet TODO: both checkpoints, correct and not
+python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e
+python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e_cl
+python test.py +experiment=test cfg_dir=../../checkpoints/moe_8e_cl2
