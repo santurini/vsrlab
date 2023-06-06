121da4c52b1332b95d3de6b1ab86055d0640a94f
Arturo Ghinassi <ghinassi.1863151@studenti.uniroma1.it>
2023-06-06 20:22:25 +0200

    testing

--- src/vsr/dataset.py
+++ src/vsr/dataset.py
@@ -85,6 +85,43 @@ class DatasetVSR(DatasetSR):
         video = [self.load_img(i) for i in video[rnd:rnd + self.seq]]
         return torch.stack(video)
 
+class ValDataset(Dataset):
+    def __init__(self,
+                 path_hr: str,
+                 path_lr: str,
+                 seq: int,
+                 hr_augmentation: ListConfig = None,
+                 lr_augmentation: ListConfig = None
+                 ):
+        super().__init__()
+        self.path_hr = list(sorted(Path(path_hr).glob('*')))
+        self.path_lr = list(sorted(Path(path_lr).glob('*')))
+        self.hr_augmentation = build_transform(hr_augmentation) if hr_augmentation else None
+        self.lr_augmentation = build_transform(lr_augmentation) if lr_augmentation else None
+        self.seq = seq
+
+    def __len__(self) -> int:
+        return len(self.path)
+
+    def __getitem__(self, index: int):
+        hr_video = list(sorted(x for x in self.path_hr[index].glob('*') if x.is_file()))
+        hr_video = self.get_frames(hr_video, randint(0, len(hr_video) - self.seq))
+        hr_video = self.hr_augmentation(hr_video)
+
+        lr_video = list(sorted(x for x in self.path_lr[index].glob('*') if x.is_file()))
+        lr_video = self.get_frames(lr_video, randint(0, len(lr_video) - self.seq))
+        lr_video = self.lr_augmentation(lr_video)
+
+        return lr_video, hr_video
+
+    @staticmethod
+    def load_img(path):
+        return to_tensor(Image.open(path))
+
+    def get_frames(self, video, rnd):
+        video = [self.load_img(i) for i in video[rnd:rnd + self.seq]]
+        return torch.stack(video)
+
 @hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="default", version_base="1.3")
 def main(cfg: omegaconf.DictConfig) -> None:
     _: Dataset = hydra.utils.instantiate(cfg.nn.data.datasets.train, _recursive_=False)
